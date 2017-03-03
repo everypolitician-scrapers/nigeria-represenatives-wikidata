748b10d3e096f0ceb795290fea111fab91bae1d1
Oliver Denman <oliver@mysociety.org>
2017-03-03 18:50:42 +0000

    Select only members of the 8th term

--- scraper.rb
+++ scraper.rb
@@ -9,7 +9,14 @@ en = WikiData::Category.new('Category:Members of the House of Representatives (N
      WikiData::Category.new('Category:Members of the House of Representatives (Nigeria) from Rivers State', 'en').member_titles |
      WikiData::Category.new('Category:Speakers of the House of Representatives (Nigeria)', 'en').member_titles
 
-sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q21290864 }'
+sparq = <<EOQ
+  SELECT ?item WHERE {
+    ?item p:P39 ?position_held .
+    ?position_held ps:P39 wd:Q21290864 ;
+                   pq:P2937 wd:Q20311259 .
+  }
+EOQ
+
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
 EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: en })
