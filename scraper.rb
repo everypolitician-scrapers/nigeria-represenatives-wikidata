bd5f9220391dbd5e1ae84e1a96a65bfb3ad3cf2f
Oliver Denman <oliver@mysociety.org>
2017-03-03 14:30:16 +0000

    Add sparql query for members of the House of Representatives of Nigeria

--- scraper.rb
+++ scraper.rb
@@ -7,7 +7,9 @@ en = WikiData::Category.new('Category:Members of the House of Representatives (N
      WikiData::Category.new('Category:Women members of the House of Representatives (Nigeria)', 'en').member_titles |
      WikiData::Category.new('Category:People\'s Democratic Party members of the House of Representatives (Nigeria)', 'en').member_titles |
      WikiData::Category.new('Category:Members of the House of Representatives (Nigeria) from Rivers State', 'en').member_titles |
-     WikiData::Category.new('Category:Speakers of the House of Representatives (Nigeria)', 'en').member_titles 
+     WikiData::Category.new('Category:Speakers of the House of Representatives (Nigeria)', 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: en })
+sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q21290864 }'
+ids = EveryPolitician::Wikidata.sparql(sparq)
 
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: en })
